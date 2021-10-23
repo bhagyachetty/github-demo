1ecb445a8f41b90ea644e28555d2663952b2f424
bhagyashree <sbhagya257@gmail.com>
2021-10-23 18:17:36 +0530

    first commit

---
+++ src/javapkg/Add.java
@@ -0,0 +1,22 @@
+package javapkg;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number a=");
+int a=sc.nextInt();
+System.out.println("enter the number b=");
+int b=sc.nextInt();
+
+//System.out.println("sum of two no= "+c);
+System.out.print("another way of doing sum of "+a+" and "+b+" is:");
+
+
+System.out.print(a+b);
+
+
+
+}
+}
